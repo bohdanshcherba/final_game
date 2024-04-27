c436cfd33f7c0641706cc5540ae2462289c91f60
Bohdan Shcherba <73034554+bohdanshcherba@users.noreply.github.com>
2024-04-27 15:58:12 +0300

    Add files via upload

---
+++ button.py
@@ -0,0 +1,23 @@
+from pygame import *
+
+class Button:
+    def __init__(self, window, screen_width, screen_height):
+        self.window = window
+        self.width = 100
+        self.height = 50
+        self.x = screen_width/2
+        self.y = screen_height/2
+
+        font.init()
+        self.font1 = font.Font(None, 20)
+
+    def reset(self): 
+        draw.rect(self.window, (255,255,255), rect.Rect( self.x,  self.y,  self.width,  self.height))
+        self.window.blit(self.font1.render('Exit', True, (0,0,0)),(self.x,  self.y,))
+
+    def click(self):
+        mouse1 = mouse.get_pos()
+        if mouse1[0] >= self.x and mouse1[0] <= self.x + self.width and mouse1[1] >= self.y and mouse1[1] <= self.y + self.height:
+            print(mouse1)
+
+
---
+++ main.py
@@ -0,0 +1,42 @@
+from pygame import *
+from button import Button
+init()
+
+info = display.Info() 
+screen_width,screen_height = info.current_w,info.current_h
+
+window = display.set_mode((screen_width,screen_height))
+background = transform.scale(image.load('background.jpg'),(screen_width,screen_height) )
+background_pause = transform.scale(image.load('pause.jpg'),(screen_width,screen_height) )
+game = True
+pause = False
+
+clock = time.Clock()
+
+btn1 = Button(window, screen_width,screen_height)
+
+while game:
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+        if e.type == KEYDOWN:
+            if e.key == K_ESCAPE:
+                pause = not pause
+        if e.type == MOUSEBUTTONDOWN:
+            btn1.click()
+
+    if pause:
+        
+        window.blit(background_pause, (0,0))
+        btn1.reset()
+
+        display.update()
+        clock.tick(60)
+        continue
+
+    window.blit(background, (0,0))
+    
+
+    display.update()
+    clock.tick(60)
